ed9ba2adca4695a87361642b8333efdb803959a6
Elinsrc <99191833+Elinsrc@users.noreply.github.com>
2026-05-23 14:29:22 +0500

    hud: watermark fix crash

--- cl_dll/ui/hud/hud_watermark.cpp
+++ cl_dll/ui/hud/hud_watermark.cpp
@@ -45,7 +45,7 @@ int CHudWatermark::Draw(float time)
 	UnpackRGB(r, g, b, gHUD.m_iDefaultHUDColor);
 
 	char str[256];
-	sprintf(str, "^2Axion^7: built ^1%s^7, commit ^2%s^7, architecture ^3%s^7, platform ^5%s\n",
+	snprintf(str, sizeof(str), "^2Axion^7: built ^1%s^7, commit ^2%s^7, architecture ^3%s^7, platform ^5%s\n",
 		BuildInfo::GetDate(),
 		BuildInfo::GetCommitHash(),
 		BuildInfo::GetArchitecture(),
@@ -55,16 +55,21 @@ int CHudWatermark::Draw(float time)
 	gHUD.DrawHudStringWithColorTags(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 2, "t.me/Elinsrc", r, g, b);
 	gHUD.DrawHudStringWithColorTags(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 3, "t.me/HalfLifeCollectiveChat", r, g, b);
 	gHUD.DrawHudStringWithColorTags(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 4, BuildInfo::GetGitHubLink(), r, g, b);
-	sprintf(str, "To disable this message, type in the console ^2hud_watermark 0");
+	snprintf(str, sizeof(str), "To disable this message, type in the console ^2hud_watermark 0");
 	gHUD.DrawHudStringWithColorTags(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 6, str, r, g, b);
 
 #if !XASH_ANDROID
     if (g_pUpdateChecker && g_pUpdateChecker->IsFinished() && g_pUpdateChecker->HasUpdate())
     {
-        gHUD.DrawHudStringWithColorTags(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 8, "^2Axion^7: ^1New update available!", r, g, b);
-		sprintf(str, "Commit: %s\n", g_pUpdateChecker->GetRemoteHash().c_str());
+        snprintf(str, sizeof(str), "^2Axion^7: ^1New update available!");
+        gHUD.DrawHudStringWithColorTags(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 8, str, r, g, b);
+		
+		std::string remoteHash = g_pUpdateChecker->GetRemoteHash();
+		snprintf(str, sizeof(str), "^7Commit: ^2%s", remoteHash.c_str());
 		gHUD.DrawHudStringWithColorTags(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 9, str, r, g, b);
-		sprintf(str, "%s\n", g_pUpdateChecker->GetCommitMessage().c_str());
+		
+		std::string commitMsg = g_pUpdateChecker->GetCommitMessage();
+		snprintf(str, sizeof(str), "^3%s", commitMsg.c_str());
 		gHUD.DrawHudStringWithColorTags(ScreenWidth / 20, gHUD.m_scrinfo.iCharHeight * 10, str, r, g, b);
     }
 #endif
