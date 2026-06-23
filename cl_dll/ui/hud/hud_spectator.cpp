ef306c9ad1c28f7e4b1d73cd051e242e4ef96507
Elinsrc <99191833+Elinsrc@users.noreply.github.com>
2026-06-23 11:23:17 +0500

    hud: spectator: restore TriAPI rendermode after overview rendering

--- cl_dll/ui/hud/hud_spectator.cpp
+++ cl_dll/ui/hud/hud_spectator.cpp
@@ -1472,6 +1472,7 @@ void CHudSpectator::DrawOverviewLayer()
 					gEngfuncs.pTriAPI->TexCoord2f( 0, 1 );
 					gEngfuncs.pTriAPI->Vertex3f( x, y + yStep, z);
 				gEngfuncs.pTriAPI->End();
+				gEngfuncs.pTriAPI->RenderMode( kRenderNormal );
 
 				frame++;
 				x += xStep;
@@ -1511,6 +1512,7 @@ void CHudSpectator::DrawOverviewLayer()
 					gEngfuncs.pTriAPI->TexCoord2f( 1, 0 );
 					gEngfuncs.pTriAPI->Vertex3f( x, y + yStep, z );
 				gEngfuncs.pTriAPI->End();
+				gEngfuncs.pTriAPI->RenderMode( kRenderNormal );
 
 				frame++;
 
@@ -1713,6 +1715,7 @@ void CHudSpectator::DrawOverviewEntities()
 		gEngfuncs.pTriAPI->TexCoord2f( 1.0f, 1.0f );
 		gEngfuncs.pTriAPI->Vertex3f( x + left[0], y + left[1], ( z + left[2] ) * zScale );
 	gEngfuncs.pTriAPI->End ();
+	gEngfuncs.pTriAPI->RenderMode( kRenderNormal );
 }
 
 void CHudSpectator::DrawOverview()
